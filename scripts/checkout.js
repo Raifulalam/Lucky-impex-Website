6a9f5528d3f3389177575255d1a4d86d54c3d41d
Raifulalam <raifulalam0123@gmail.com>
2024-06-26 15:55:42 +0545

    make delete button interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,7 +14,8 @@ cart.forEach((cartItem) => {
     });
     cartSummaryHTML +=
         `
- <div class="cart-item-container">
+ <div class="cart-item-container
+  js-cart-item-container-${matchingProducts.id}">
                     <div class="delivery-date">
                         Delivery date: Tuesday, June 21
                     </div>
@@ -92,8 +93,10 @@ document.querySelectorAll('.js-delete-link')
         link.addEventListener('click', (event) => {
             const productId = link.dataset.productId;
             removeFromCart(productId);
-            console.log(cart);
 
+            const container = document.querySelector(`.js-cart-item-container-${productId}`);
+            container.remove();
         });
 
     });
+
