2b612a0f997ac2a737c1392e358652df7e737aca
Raifulalam <raifulalam0123@gmail.com>
2024-06-26 15:02:37 +0545

    fix radio selector

--- Data/products.js
+++ Data/products.js
@@ -19,13 +19,13 @@ export const products = [
     },
     {
         id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        image: "images/products/intermediate-composite-basketball.jpg",
+        image: "front-image/RT28A32216R.png",
         name: "Intermediate Size Basketball",
         rating: {
             stars: 4,
             count: 127
         },
-        priceCents: 2095,
+        price: 2095,
         keywords: [
             "sports",
             "basketballs"
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -45,7 +45,7 @@ cart.forEach((cartItem) => {
                                 Choose a delivery option:
                             </div>
                             <div class="delivery-option">
-                                <input type="radio" checked class="delivery-option-input" name="delivery-option-1">
+                                <input type="radio" checked class="delivery-option-input" name="delivery-option-${matchingProducts.id}">
                                 <div>
                                     <div class="delivery-option-date">
                                         Tuesday, June 21
@@ -56,7 +56,7 @@ cart.forEach((cartItem) => {
                                 </div>
                             </div>
                             <div class="delivery-option">
-                                <input type="radio" class="delivery-option-input" name="delivery-option-1">
+                                <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProducts.id}">
                                 <div>
                                     <div class="delivery-option-date">
                                         Wednesday, June 15
@@ -67,7 +67,7 @@ cart.forEach((cartItem) => {
                                 </div>
                             </div>
                             <div class="delivery-option">
-                                <input type="radio" class="delivery-option-input" name="delivery-option-1">
+                                <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProducts.id}">
                                 <div>
                                     <div class="delivery-option-date">
                                         Monday, June 13
@@ -79,6 +79,76 @@ cart.forEach((cartItem) => {
                             </div>
                         </div>
                     </div>
-                </div>`
+                </div>
+                 <div class="cart-item-container">
+                    <div class="delivery-date">
+                        Delivery date: Wednesday, June 15
+                    </div>
+
+                    <div class="cart-item-details-grid">
+                        <img class="product-image" src="">
+
+                        <div class="cart-item-details">
+                            <div class="product-name">
+                                Intermediate Size Basketball
+                            </div>
+                            <div class="product-price">
+                                $20.95
+                            </div>
+                            <div class="product-quantity">
+                                <span>
+                                    Quantity: <span class="quantity-label">1</span>
+                                </span>
+                                <span class="update-quantity-link link-primary">
+                                    Update
+                                </span>
+                                <span class="delete-quantity-link link-primary">
+                                    Delete
+                                </span>
+                            </div>
+                        </div>
+
+                        <div class="delivery-options">
+                            <div class="delivery-options-title">
+                                Choose a delivery option:
+                            </div>
+
+                            <div class="delivery-option">
+                                <input type="radio" class="delivery-option-input" name="delivery-option-2">
+                                <div>
+                                    <div class="delivery-option-date">
+                                        Tuesday, June 21
+                                    </div>
+                                    <div class="delivery-option-price">
+                                        FREE Shipping
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="delivery-option">
+                                <input type="radio" checked class="delivery-option-input" name="delivery-option-2">
+                                <div>
+                                    <div class="delivery-option-date">
+                                        Wednesday, June 15
+                                    </div>
+                                    <div class="delivery-option-price">
+                                        $4.99 - Shipping
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="delivery-option">
+                                <input type="radio" class="delivery-option-input" name="delivery-option-2">
+                                <div>
+                                    <div class="delivery-option-date">
+                                        Monday, June 13
+                                    </div>
+                                    <div class="delivery-option-price">
+                                        $9.99 - Shipping
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+`
 });
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
