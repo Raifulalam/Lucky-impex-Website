4584d03633b6d08c76e97ea6e013849ea4413697
Raifulalam <raifulalam0123@gmail.com>
2024-06-27 16:18:56 +0545

    add jasmin texting framework

--- Data/products.js
+++ Data/products.js
@@ -37,7 +37,8 @@ export const products = [
             stars: 4,
             count: 127
         },
-        price: 2095,
+        mrp: 60458,
+        price: 56095,
         keywords: [
             "sports",
             "basketballs"
@@ -45,13 +46,14 @@ export const products = [
     },
     {
         id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-        image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+        image: "image/Single-door/RR20T282ZU8(2).png",
         name: "Adults Plain Cotton T-Shirt - 2 Pack",
         rating: {
             stars: 4.5,
             count: 56
         },
-        priceCents: 799,
+        mrp: 69453,
+        price: 63099,
         keywords: [
             "tshirts",
             "apparel",
@@ -62,13 +64,14 @@ export const products = [
     },
     {
         id: "54e0eccd-8f36-462b-b68a-8182611d9add",
-        image: "images/products/black-2-slot-toaster.jpg",
+        image: "image/Single-door/8071322C7EC07CC-RR20C20C2GS.png",
         name: "2 Slot Toaster - Black",
         rating: {
             stars: 5,
-            count: 2197
+            count: 219
         },
-        priceCents: 1899,
+        mrp: 56102,
+        price: 48899,
         keywords: [
             "toaster",
             "kitchen",
