8b490d8efb3db037d7d145320428862f2963fc4a
Raifulalam <raifulalam0123@gmail.com>
2024-06-30 18:42:06 +0545

    make local =Storage key and from private

--- Data/cart-class.js
+++ Data/cart-class.js
@@ -1,13 +1,13 @@
 class Cart {
     cartItems;
-    localStorageKey;
+    #localStorageKey;
     constructor(localStorageKey) {
-        this.localStorageKey = localStorageKey;
-        this.loadFromStorage();
+        this.#localStorageKey = localStorageKey;
+        this.#loadFromStorage();
 
     }
-    loadFromStorage() {
-        this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+    #loadFromStorage() {
+        this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
         if (!this.cartItems) {
             this.cartItems = [{
                 productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
@@ -23,7 +23,7 @@ class Cart {
 
     }
     saveToStorage() {
-        localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+        localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
     }
     addToCart(productId) {
         let matchingItem;
